60ed8f8db78092c44b12bb981a268290f89d9cc0
frank51d <frank52xd@gmail.com>
2020-02-12 11:01:02 -0400

    v16

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ class Server {
             next();
         });
         this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(express.urlencoded({ extended:false }));
 
     }
 
